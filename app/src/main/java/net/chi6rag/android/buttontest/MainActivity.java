c491d4dec3574053a383e25224c2de6009b43366
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-22 19:51:10 +0530

    Refactor Code

--- app/src/main/java/net/chi6rag/android/buttontest/MainActivity.java
+++ app/src/main/java/net/chi6rag/android/buttontest/MainActivity.java
@@ -7,8 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener{
     Button button_learn_more;
+    Button button_share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,25 @@ public class MainActivity extends Activity implements OnClickListener {
         setContentView(R.layout.activity_main);
         button_learn_more = (Button) findViewById(R.id.button_learn_more);
         button_learn_more.setOnClickListener(this);
+        button_share = (Button) findViewById(R.id.button_share);
+        button_share.setOnClickListener(this);
     }
 
     public void onClick(View view){
-        Log.d("chi6rag", "Learn More button is clicked");
+        switch(view.getId()){
+            case R.id.button_learn_more:
+                Log.i("chi6rag", "Learn More button was clicked");
+                break;
+            case R.id.button_teach:
+                Log.i("chi6rag", "Teach button was clicked");
+                break;
+            case R.id.button_share:
+                Log.i("chi6rag", "Teach button was clicked");
+                break;
+            default:
+                Log.e("chi6rag", "Unidentified button clicked");
+                break;
+        }
     }
 
     public void handleClick(View view){
