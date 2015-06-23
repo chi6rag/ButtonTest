648cbaa8b5e522524a4e5dc7635f8055361e409d
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-23 09:27:53 +0530

    Add code for anonymous inner class button handler

--- app/src/main/java/net/chi6rag/android/buttontest/MainActivity.java
+++ app/src/main/java/net/chi6rag/android/buttontest/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity{
     Button button_click_inner_class;
+    Button button_click_anonymous_inner_class_1;
+    Button button_click_anonymous_inner_class_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,28 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
         button_click_inner_class = (Button) findViewById(R.id.button_click_inner_class);
         button_click_inner_class.setOnClickListener(new ButtonActivity());
+
+        button_click_anonymous_inner_class_1 = (Button) findViewById
+                (R.id.button_click_anonymous_inner_class_1);
+        button_click_anonymous_inner_class_1.setOnClickListener(ocl);
+
+        button_click_anonymous_inner_class_2 = (Button) findViewById
+                (R.id.button_click_anonymous_inner_class_2);
+        button_click_anonymous_inner_class_2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("chi6rag", "Clicked button_click_anonymous_inner_class_2");
+            }
+        });
+
     }
+
+    OnClickListener ocl = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("chi6rag", "Clicked button_click_anonymous_inner_class_1");
+        }
+    };
 
     public class ButtonActivity implements OnClickListener{
 
